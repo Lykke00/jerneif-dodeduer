0575d05b4c4e16dbbabcdfe68e8432199640b358
Lykke <lykber01@easv365.dk>
2025-11-22 17:22:54 +0100

    tilføjet heroUI og tailwindCSS

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './globals.css';
+import IndexPage from './pages/index/page';
+import { HeroUIProvider } from '@heroui/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <HeroUIProvider>
+      <IndexPage />
+    </HeroUIProvider>
+  </StrictMode>
+);
---
+++ client/src/pages/index/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@heroui/react';
+
+export default function IndexPage() {
+  return (
+    <div>
+      <Button>dette er en knap</Button>
+    </div>
+  );
+}
