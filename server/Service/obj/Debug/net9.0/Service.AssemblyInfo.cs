ccb834bcfa4fc9466da3d9905eb90875929d7c95
based4real <lykber01@easv365.dk>
2025-12-04 22:25:35 +0100

    tilføjet relevante pakker

--- server/Service/obj/Debug/net9.0/Service.AssemblyInfo.cs
+++ server/Service/obj/Debug/net9.0/Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8798b07210b3f05d6a125612979c485532f1a1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c975c7e5143372880865750ebe4e0edeeec3f29b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
