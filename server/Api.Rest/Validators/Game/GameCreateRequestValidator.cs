728c6c6a32528b0bcb4cecd48fbe89189236da7d
Lykke00 <lykber01@easv365.dk>
2025-12-17 12:28:54 +0100

    tilføjet 2 validators til updategame og creategame

---
+++ server/Api.Rest/Validators/Game/GameCreateRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Service.DTO.Game;
+
+namespace Api.Rest.Validators.Game;
+
+public class GameCreateRequestValidator : AbstractValidator<GameCreateRequest>
+{
+    public GameCreateRequestValidator()
+    {
+        RuleFor(x => x.WeekNumber)
+            .GreaterThan(0).WithMessage("Week number must be greater than 0");
+        
+        
+    }
+}
---
+++ server/Api.Rest/Validators/Game/GameUpdateRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Service.DTO.Game;
+
+namespace Api.Rest.Validators.Game;
+
+public class GameUpdateRequestValidator : AbstractValidator<GameUpdateRequest>
+{
+    public GameUpdateRequestValidator()
+    {
+        RuleFor(x => x.WinningNumbers)
+            .NotNull().WithMessage("Winning numbers must not be null")
+            .Must(numbers => numbers.Count == 3).WithMessage("There must be exactly 3 winning numbers")
+            .Must(numbers => numbers.All(n => n >= 1 && n <= 16)).WithMessage("Winning numbers must be between 1 and 16")
+            .Must(numbers => numbers.Distinct().Count() == numbers.Count).WithMessage("Winning numbers must be unique");
+    }
+}
